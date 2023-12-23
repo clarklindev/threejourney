4112136a214c248bb356fcf6960bebd8c4639901
clarklindev <clarklin.dev@gmail.com>
2023-12-23 12:34:18 +0800

    TextGeometry

--- README.md
+++ README.md
@@ -815,3 +815,41 @@ https://matheowis.github.io/HDRI-to-CubeMap
 
 - save as separate file versions
 - default format downloaded is png
+
+### 3D Text
+
+- NB: TextGeometry and FontLoader have been moved out of the core some time ago so you have to import them
+- TextGeometry
+- need a font called typeface
+- convert webfont to typeface - gero3.github.io/facetype.js
+- also: node_modules/three/examples/fonts/[typefacefont] : DONT DO THIS!  
+   import typefacefont from 'three/examples/fonts/helvetiker_regular.typeface.json'
+- drop the font and license into assets folder: static/fonts etc (see what this is for vite)
+- use FontLoader to handle loading: https://threejs.org/docs/#examples/en/loaders/FontLoader
+- FontLoader uses a callback - loaded font received as prop in callback
+
+```js
+import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
+import { FontLoader } from "three/addons/loaders/FontLoader.js";
+
+const fontLoader = new FontLoader();
+
+fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
+  console.log("font loaded");
+  const textGeometry = new TextGeometry("helloworld", {
+    font: font,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 5,
+  });
+
+  const textMaterial = new THREE.MeshBasicMaterial();
+  const text = new THREE.Mesh(textGeometry, textMaterial);
+  scene.add(text);
+});
+```
--- script.js
+++ script.js
@@ -3,6 +3,8 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
+import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
+import { FontLoader } from "three/addons/loaders/FontLoader.js";
 
 //DEBUG
 const gui = new dat.GUI();
@@ -136,14 +138,14 @@ doorColorTexture.colorSpace = THREE.SRGBColorSpace;
 matcapTexture.colorSpace = THREE.SRGBColorSpace;
 
 //loads front,back,left, right,top,bottom - order important
-const environmentMapTexture = cubeTextureLoader.load([
-  "/textures/environmentMaps/3/px.jpg",
-  "/textures/environmentMaps/3/nx.jpg",
-  "/textures/environmentMaps/3/py.jpg",
-  "/textures/environmentMaps/3/ny.jpg",
-  "/textures/environmentMaps/3/pz.jpg",
-  "/textures/environmentMaps/3/nz.jpg",
-]);
+// const environmentMapTexture = cubeTextureLoader.load([
+//   "/textures/environmentMaps/3/px.jpg",
+//   "/textures/environmentMaps/3/nx.jpg",
+//   "/textures/environmentMaps/3/py.jpg",
+//   "/textures/environmentMaps/3/ny.jpg",
+//   "/textures/environmentMaps/3/pz.jpg",
+//   "/textures/environmentMaps/3/nz.jpg",
+// ]);
 
 //MATERIAL
 
@@ -182,64 +184,86 @@ const environmentMapTexture = cubeTextureLoader.load([
 // material.gradientMap = gradientTexture;
 
 //MESHSTANDARD Material / ambient occlusion
-const material = new THREE.MeshStandardMaterial();
-// material.metalness = 0;
-// material.roughness = 1;
-// material.map = doorColorTexture;
-// material.aoMap = doorAmbientOcclusionTexture;
-// material.aoMapIntensity = 1;
-// material.displacementMap = doorHeightTexture;
-// material.displacementScale = 0.05;
-// material.metalnessMap = doorMetalnessTexture;
-// material.roughnessMap = doorRoughnessTexture;
-// material.normalMap = doorNormalTexture;
-// material.normalScale.set(1.5, 1.5);
-// material.transparent = true;
-// material.alphaMap = doorAlphaTexture;
+// const material = new THREE.MeshStandardMaterial();
+// // material.metalness = 0;
+// // material.roughness = 1;
+// // material.map = doorColorTexture;
+// // material.aoMap = doorAmbientOcclusionTexture;
+// // material.aoMapIntensity = 1;
+// // material.displacementMap = doorHeightTexture;
+// // material.displacementScale = 0.05;
+// // material.metalnessMap = doorMetalnessTexture;
+// // material.roughnessMap = doorRoughnessTexture;
+// // material.normalMap = doorNormalTexture;
+// // material.normalScale.set(1.5, 1.5);
+// // material.transparent = true;
+// // material.alphaMap = doorAlphaTexture;
 
-material.metalness = 0.7;
-material.roughness = 0.2;
-material.envMap = environmentMapTexture;
+// material.metalness = 0.7;
+// material.roughness = 0.2;
+// material.envMap = environmentMapTexture;
 
-gui.add(material, "metalness").min(0).max(1).step(0.0001);
-gui.add(material, "roughness").min(0).max(1).step(0.0001);
-gui.add(material, "aoMapIntensity").min(0).max(10).step(0.001);
-gui.add(material, "displacementScale").min(0).max(1).step(0.001);
+// gui.add(material, "metalness").min(0).max(1).step(0.0001);
+// gui.add(material, "roughness").min(0).max(1).step(0.0001);
+// gui.add(material, "aoMapIntensity").min(0).max(10).step(0.001);
+// gui.add(material, "displacementScale").min(0).max(1).step(0.001);
 
-//sphere
-const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 64, 64), material);
-sphere.position.x = -1.5;
+// //sphere
+// const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 64, 64), material);
+// sphere.position.x = -1.5;
 
-//plane
-const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1, 100, 100), material);
+// //plane
+// const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1, 100, 100), material);
 
-//torus
-const torus = new THREE.Mesh(
-  new THREE.TorusGeometry(0.3, 0.2, 64, 128),
-  material
-);
-torus.position.x = 1.5;
+// //torus
+// const torus = new THREE.Mesh(
+//   new THREE.TorusGeometry(0.3, 0.2, 64, 128),
+//   material
+// );
+// torus.position.x = 1.5;
 
-sphere.castShadow = true;
-plane.castShadow = true;
-torus.castShadow = true;
-plane.receiveShadow = true;
-scene.add(sphere, plane, torus);
+// sphere.castShadow = true;
+// plane.castShadow = true;
+// torus.castShadow = true;
+// plane.receiveShadow = true;
+// scene.add(sphere, plane, torus);
 
-sphere.geometry.setAttribute(
-  "uv2",
-  new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2)
-);
+// sphere.geometry.setAttribute(
+//   "uv2",
+//   new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2)
+// );
 
-plane.geometry.setAttribute(
-  "uv2",
-  new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2)
-);
+// plane.geometry.setAttribute(
+//   "uv2",
+//   new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2)
+// );
 
-torus.geometry.setAttribute(
-  "uv2",
-  new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2)
-);
+// torus.geometry.setAttribute(
+//   "uv2",
+//   new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2)
+// );
+
+//FONTS
+const fontLoader = new FontLoader();
+
+fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
+  console.log("font loaded");
+  const textGeometry = new TextGeometry("helloworld", {
+    font: font,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 5,
+  });
+
+  const textMaterial = new THREE.MeshBasicMaterial();
+  const text = new THREE.Mesh(textGeometry, textMaterial);
+  scene.add(text);
+});
 
 //---------------------------------------------------------
 //LIGHTING
@@ -261,13 +285,13 @@ const clock = new THREE.Clock();
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
-  sphere.rotation.y = 0.1 * elapsedTime;
-  plane.rotation.y = 0.1 * elapsedTime;
-  torus.rotation.y = 0.1 * elapsedTime;
+  // sphere.rotation.y = 0.1 * elapsedTime;
+  // plane.rotation.y = 0.1 * elapsedTime;
+  // torus.rotation.y = 0.1 * elapsedTime;
 
-  sphere.rotation.x = -0.15 * elapsedTime;
-  plane.rotation.x = -0.15 * elapsedTime;
-  torus.rotation.x = -0.15 * elapsedTime;
+  // sphere.rotation.x = -0.15 * elapsedTime;
+  // plane.rotation.x = -0.15 * elapsedTime;
+  // torus.rotation.x = -0.15 * elapsedTime;
 
   // Render
   renderer.render(scene, camera);
