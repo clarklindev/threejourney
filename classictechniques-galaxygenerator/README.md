86beddbed8c9479b92407e32bf0450188d84c63f
clarklindev <clarklin.dev@gmail.com>
2023-12-31 01:52:00 +0800

    galaxy spin

--- classictechniques-galaxygenerator/README.md
+++ classictechniques-galaxygenerator/README.md
@@ -23,3 +23,38 @@ position[i3 + 0] = radius;
 position[i3 + 1] = 0;
 position[i3 + 2] = 0;
 ```
+
+#### galaxy branches
+
+- branches should be equidistant from each other
+- to calculate which branch the star should go, calculate branch = star modular the number of branches (eg. 100 % 3)
+- divide by branches to give number between 0 and 1 (circle ratio)
+- half circle is Math.PI
+- full circle is 2 Math.PI (to get angle, we just multiply ratio by 2PI)
+- to position things on the circle, use cos and sin on the axes (by default the radius is 1)
+
+```js
+const radius = Math.random() * parameters.radius;
+
+const branchAngle =
+  ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
+
+positions[i3 + 0] = Math.cos(branchAngle) * radius;
+positions[i3 + 1] = 0;
+positions[i3 + 2] = Math.sin(branchAngle) * radius;
+```
+
+#### Spin
+
+- particles (stars) closer to center of galaxy should move (spin) slower than edge
+
+```js
+parameters.spin = 1;
+
+//for each star
+const spinAngle = radius * parameters.spin;
+
+positions[i3 + 0] = Math.cos(branchAngle + spinAngle) * radius;
+positions[i3 + 1] = 0;
+positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius;
+```
--- classictechniques-galaxygenerator/src/main.ts
+++ classictechniques-galaxygenerator/src/main.ts
@@ -20,7 +20,9 @@ const scene = new THREE.Scene();
 const parameters = {
   count: 100000,
   size: 0.01,
-  radius: 5
+  radius: 5,
+  branches: 3,
+  spin: 1,
 }
 
 let geometry: THREE.BufferGeometry | null;
@@ -41,12 +43,17 @@ const generateGalaxy = () => {
 
   for (let i =0; i< parameters.count; i++){
     const i3 = i * 3;
-    
+
     const radius = Math.random() * parameters.radius;
-    positions[i3 + 0] = radius;
+    const spinAngle = radius * parameters.spin;
+
+    const branchAngle = ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
+
+    positions[i3 + 0] = Math.cos(branchAngle + spinAngle) * radius;
     positions[i3 + 1] = 0;
-    positions[i3 + 2] = 0;
+    positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius;
   }
+
   geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
 
   //MATERIAL
@@ -66,6 +73,8 @@ generateGalaxy();
 gui.add(parameters, "count").min(100).max(10000).step(100).onFinishChange(generateGalaxy);
 gui.add(parameters, "size").min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy);
 gui.add(parameters, "radius").min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy);
+gui.add(parameters, "branches").min(2).max(20).step(1).onFinishChange(generateGalaxy);
+gui.add(parameters, "spin").min(-5).max(5).step(0.001).onFinishChange(generateGalaxy);
 /**
  * Sizes
  */
