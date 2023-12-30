4a3c8ed256448c79623afdb05f06d3bb7d978dae
clarklindev <clarklin.dev@gmail.com>
2023-12-31 00:31:31 +0800

    placing items on straight line

--- classictechniques-galaxygenerator/README.md
+++ classictechniques-galaxygenerator/README.md
@@ -5,5 +5,21 @@
 - need to cleanup old galaxies from scene
 
 ```js
+// destroy old galaxy
+if (points !== null) {
+  geometry?.dispose(); //free memory for geometry
+  material?.dispose();
+  scene.remove(points); //clearing points from scene
+}
+```
 
+- to create galaxy:
+
+  1. place stars on straight line - define a radius, then place stars between 0 and the radius
+
+```js
+const radius = Math.random() * parameters.radius;
+position[i3 + 0] = radius;
+position[i3 + 1] = 0;
+position[i3 + 2] = 0;
 ```
--- classictechniques-galaxygenerator/src/main.ts
+++ classictechniques-galaxygenerator/src/main.ts
@@ -20,6 +20,7 @@ const scene = new THREE.Scene();
 const parameters = {
   count: 100000,
   size: 0.01,
+  radius: 5
 }
 
 let geometry: THREE.BufferGeometry | null;
@@ -40,10 +41,11 @@ const generateGalaxy = () => {
 
   for (let i =0; i< parameters.count; i++){
     const i3 = i * 3;
-
-    positions[i + 0] = (Math.random() - 0.5) * 3; //x
-    positions[i + 1] = (Math.random() - 0.5) * 3; //y
-    positions[i + 2] = (Math.random() - 0.5) * 3; //z
+    
+    const radius = Math.random() * parameters.radius;
+    positions[i3 + 0] = radius;
+    positions[i3 + 1] = 0;
+    positions[i3 + 2] = 0;
   }
   geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
 
@@ -63,7 +65,7 @@ const generateGalaxy = () => {
 generateGalaxy();
 gui.add(parameters, "count").min(100).max(10000).step(100).onFinishChange(generateGalaxy);
 gui.add(parameters, "size").min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy);
-
+gui.add(parameters, "radius").min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy);
 /**
  * Sizes
  */
