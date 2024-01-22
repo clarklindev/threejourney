eda2f45ed0121004a6281a2f5d70b942f5f46f9f
clarklindev <clarklindev@gmail.com>
2024-01-22 16:24:28 +0800

    28-shaders-shaderpatterns-pattern14

--- 28-shaders-shaderpatterns/src/shaders/test/fragment.glsl
+++ 28-shaders-shaderpatterns/src/shaders/test/fragment.glsl
@@ -119,15 +119,16 @@ void main()
     // gl_FragColor = (vec4(vec3(strength), 1));
 
     // // Pattern 13 - variation of pattern 12 - change the strength of step limit - so value is 1 (white) earlier - ie lower the value on 
-    float strength = step(0.4, mod(vUv.x * 10.0, 1.0));  //vertical lines
-    strength *= step(0.8, mod(vUv.y * 10.0, 1.0));  //horizontal lines
-    gl_FragColor = (vec4(vec3(strength), 1));
+    // float strength = step(0.4, mod(vUv.x * 10.0, 1.0));  //vertical lines
+    // strength *= step(0.8, mod(vUv.y * 10.0, 1.0));  //horizontal lines
+    // gl_FragColor = (vec4(vec3(strength), 1));
 
-    // // Pattern 14
-    // float barX = step(0.4, mod(vUv.x * 10.0, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
-    // float barY = step(0.8, mod(vUv.x * 10.0, 1.0)) * step(0.4, mod(vUv.y * 10.0, 1.0));
-    // float strength = barX + barY;
-    // strength = clamp(strength, 0.0, 1.0);
+    // // Pattern 14 - combination of pattern 13 on both horizontal and vertical
+    float barX = step(0.4, mod(vUv.x * 10.0, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
+    float barY = step(0.8, mod(vUv.x * 10.0, 1.0)) * step(0.4, mod(vUv.y * 10.0, 1.0));
+    float strength = barX + barY;
+    strength = clamp(strength, 0.0, 1.0);
+    gl_FragColor = (vec4(vec3(strength), 1));
 
     // // Pattern 15
     // float barX = step(0.4, mod(vUv.x * 10.0 - 0.2, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
