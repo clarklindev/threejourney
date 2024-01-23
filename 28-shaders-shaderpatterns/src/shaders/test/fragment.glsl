75ad7921fb7206134b408e3c0028f32532a05e40
clarklindev <clarklindev@gmail.com>
2024-01-23 16:07:47 +0800

    28-shaders-shaderpatterns-pattern38-applying-sin()-to-vUv.y-and-vUv.x

--- 28-shaders-shaderpatterns/src/shaders/test/fragment.glsl
+++ 28-shaders-shaderpatterns/src/shaders/test/fragment.glsl
@@ -277,19 +277,20 @@ void main()
     // gl_FragColor = vec4(vec3(strength), 1);   
 
     // // Pattern 37 - applying sin() to a vUv.y
+    // vec2 wavedUv = vec2(
+    //     vUv.x,
+    //     vUv.y + sin(vUv.x * 30.0) * 0.1
+    // );
+    // float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
+    // gl_FragColor = vec4(vec3(strength), 1);   
+
+    // // Pattern 38 - applying sin() on vUv.x and vUv.y (variation of pattern37)
     vec2 wavedUv = vec2(
-        vUv.x,
+        vUv.x + sin(vUv.y * 30.0) * 0.1,
         vUv.y + sin(vUv.x * 30.0) * 0.1
     );
     float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
     gl_FragColor = vec4(vec3(strength), 1);   
-
-    // // Pattern 38
-    // vec2 wavedUv = vec2(
-    //     vUv.x + sin(vUv.y * 30.0) * 0.1,
-    //     vUv.y + sin(vUv.x * 30.0) * 0.1
-    // );
-    // float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
 
     // // Pattern 39
     // vec2 wavedUv = vec2(
