57435a0fa458078aa79466f3cfa231fa29b066ff
clarklindev <clarklindev@gmail.com>
2024-01-23 15:11:29 +0800

    28-shaders-shaderpatterns-pattern32-rotated-pattern31-using-rotate-helper

--- 28-shaders-shaderpatterns/src/shaders/test/fragment.glsl
+++ 28-shaders-shaderpatterns/src/shaders/test/fragment.glsl
@@ -1,3 +1,4 @@
+//define is like a const
 #define PI 3.1415926535897932384626433832795
 
 varying vec2 vUv;
@@ -8,6 +9,7 @@ float random(vec2 st)
     return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
 }
 
+//Pattern 32 rotate vUv
 vec2 rotate(vec2 uv, float rotation, vec2 mid)
 {
     return vec2(
@@ -215,25 +217,44 @@ void main()
     // gl_FragColor = vec4(vec3(strength), 1);   
 
     // // Pattern 31 - star (diamond with gradient) - pattern30 (stretched sun) on vertical AND horizontal
-    vec2 lightUvX = vec2(
-      vUv.x, 
-      (vUv.y - 0.5) * 5.0 + 0.5
-    );
-    float lightX = 0.15 / (distance(lightUvX, vec2(0.5)));
+    // vec2 lightUvX = vec2(
+    //   vUv.x, 
+    //   (vUv.y - 0.5) * 5.0 + 0.5
+    // );
+    // float lightX = 0.15 / (distance(lightUvX, vec2(0.5)));
 
-    vec2 lightUvY = vec2(
-      vUv.y, 
-      (vUv.x - 0.5) * 5.0 + 0.5
-    );
-    float lightY = 0.15 / (distance(lightUvY, vec2(0.5)));
+    // vec2 lightUvY = vec2(
+    //   vUv.y, 
+    //   (vUv.x - 0.5) * 5.0 + 0.5
+    // );
+    // float lightY = 0.15 / (distance(lightUvY, vec2(0.5)));
 
-    float strength = lightX * lightY;
-    gl_FragColor = vec4(vec3(strength), 1);   
+    // float strength = lightX * lightY;
+    // gl_FragColor = vec4(vec3(strength), 1);   
 
-    // // Pattern 32
-    // vec2 rotatedUv = rotate(vUv, PI * 0.25, vec2(0.5));
+    // // Pattern 32 - rotated pattern 31 - rotate vUv coordinates in the center (use helper function rotate())
+    //pie is half a circle, so we only rotate 1/4 of pie
+    
+    //ANSWER- METHOD1
+    // vec2 rotatedUv = rotate(vUv, PI * 0.25, vec2(0.5)); //rotate(point, angle, pivotpoint)
     // float strength = 0.15 / (distance(vec2(rotatedUv.x, (rotatedUv.y - 0.5) * 5.0 + 0.5), vec2(0.5)));
     // strength *= 0.15 / (distance(vec2(rotatedUv.y, (rotatedUv.x - 0.5) * 5.0 + 0.5), vec2(0.5)));
+    // gl_FragColor = vec4(vec3(strength), 1);  
+
+    //ANSWER- METHOD2
+    vec2 rotatedUv = rotate(vUv, PI * 0.25, vec2(0.5)); //rotate(point, angle, pivotpoint)
+    vec2 lightUvX = vec2(
+      rotatedUv.x, 
+      (rotatedUv.y - 0.5) * 5.0 + 0.5
+    );
+    float lightX = 0.15 / (distance(lightUvX, vec2(0.5)));
+    vec2 lightUvY = vec2(
+      rotatedUv.y, 
+      (rotatedUv.x - 0.5) * 5.0 + 0.5
+    );
+    float lightY = 0.15 / (distance(lightUvY, vec2(0.5)));
+    float strength = lightX * lightY;
+    gl_FragColor = vec4(vec3(strength), 1);   
 
     // // Pattern 33
     // float strength = step(0.5, distance(vUv, vec2(0.5)) + 0.25);
