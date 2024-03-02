993dc351ff0fd607daa32ff993173e7b2ad14dfe
clarklindev <clarklindev@gmail.com>
2024-03-02 10:18:48 +0800

    53-r3f-game-BlockStart-props

--- 53-react-three-fiber-create-a-game/src/Level.jsx
+++ 53-react-three-fiber-create-a-game/src/Level.jsx
@@ -1,12 +1,14 @@
-const BlockStart = () =>{
-  return <mesh position={[0, -0.1, 0]} receiveShadow>
-    <boxGeometry args={[4, 0.2, 4]}/>
-    <meshStandardMaterial color="limegreen"/>
-  </mesh>
+const BlockStart = ({position=[0,0,0]}) =>{
+  return <group position={position}>  
+    <mesh position={[0, -0.1, 0]} receiveShadow>
+      <boxGeometry args={[4, 0.2, 4]}/>
+      <meshStandardMaterial color="limegreen"/>
+    </mesh>
+  </group>
 }
 
 const Level = ()=>{
-  return <BlockStart/>
+  return <BlockStart position={[0,0,0]}/>
 }
 
 export default Level;
