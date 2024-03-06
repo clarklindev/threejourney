27c44a220df6adbb590bec53519de643ad5307b4
clarklindev <clarklindev@gmail.com>
2024-03-06 13:02:16 +0800

    34-extra-intro-and-loading-progress

--- 34-extra-intro-and-loading-progress/src/script.js
+++ 34-extra-intro-and-loading-progress/src/script.js
@@ -1,4 +1,3 @@
-import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
